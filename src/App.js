bc8a5032aace8396e89c1855837090479dc31790
Goran <pavlovskigoran88@gmail.com>
2019-12-28 21:47:42 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      welcomeText: 'Hello, Goran',
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.welcomeText}</p>
+          <button onClick={() => this.setState({ welcomeText: 'Name is changed' })}>Change Name</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
