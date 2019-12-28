315b2220e0c09506c19a9280cfe69e579ff7afca
Goran <pavlovskigoran88@gmail.com>
2019-12-28 22:19:19 +0100

    did mount lifecycle and fetch a data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      welcomeText: 'Hello, Goran',
+      monsters: [],
     };
   }
+
+  componentDidMount() {
+    console.info('mounted');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then((res) => this.setState({monsters: res}))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.welcomeText}</p>
-          <button onClick={() => this.setState({ welcomeText: 'Name is changed' })}>Change Name</button>
-        </header>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
