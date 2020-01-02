0e3f7f205d5e4a59b744e6fa99380ce680e08e94
Goran <pavlovskigoran88@gmail.com>
2020-01-02 16:21:23 +0100

    Updates

---
+++ precache-manifest.0b22b95b3a9a06be717254b82707cb0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bedb89a322ff37ecb5b1d7a066ce188",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4a92e036c3d3928ace29",
+    "url": "/monsters-rolodex/static/css/main.a2c520f9.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a92e036c3d3928ace29",
+    "url": "/monsters-rolodex/static/js/main.2b53af5f.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
